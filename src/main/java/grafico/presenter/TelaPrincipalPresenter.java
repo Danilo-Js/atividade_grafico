03f30f882fbe574e3d22ac72d60b59e31152b931
Danilo J. L. de Oliveira <danilojldeo@hotmail.com>
2022-07-11 21:48:10 -0300

    Start: Listeners dos RadioButtons

--- src/main/java/grafico/presenter/TelaPrincipalPresenter.java
+++ src/main/java/grafico/presenter/TelaPrincipalPresenter.java
@@ -8,7 +8,52 @@ public class TelaPrincipalPresenter {
     public TelaPrincipalPresenter() {
         this.view = new TelaPrincipal();
         
-        //  Listeners
+        // Elementos Gráficos
+        // Título
+        this.view.getRadio_Titulo().addActionListener((e) -> {
+
+        });
+        
+        // Legenda
+        this.view.getRadio_Legenda().addActionListener((e) -> {
+
+        });
+        
+        // Título dos eixos
+        this.view.getRadio_TituloDosEixos().addActionListener((e) -> {
+            
+        });
+        
+        // Rótulo de dados (%)
+        this.view.getRadio_RotuloDeDadosPercent().addActionListener((e) -> {
+            
+        });
+        
+        // Rótulo de dados - Valor
+        this.view.getRadio_RotuloDeDadosValor().addActionListener((e) -> {
+            
+        });
+        
+        // Rótulo de dados - Valor (%)
+        this.view.getRadio_RotuloDeDadosValorPercent().addActionListener((e) -> {
+            
+        });
+        
+        // Cor das barras
+        this.view.getRadio_CorDasBarras().addActionListener((e) -> {
+            
+        });
+        
+        // Cor das barras (por grupo)
+        this.view.getRadio_CorDasBarrasPorGrupo().addActionListener((e) -> {
+            
+        });
+        
+        // Grade
+        this.view.getRadio_Grade().addActionListener((e) -> {
+            
+        });
+        
         
         this.view.setVisible(true);
     }
